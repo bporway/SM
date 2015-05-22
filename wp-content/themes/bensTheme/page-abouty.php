7c20e6b177d14afa10b740207412163318fff8d6
Ben Porway <benporway@gmail.com>
2015-05-22 13:37:17 -0400

    about page second test. not sure about htaccess

---
+++ wp-content/themes/bensTheme/page-abouty.php
@@ -0,0 +1,107 @@
+<?php while (have_posts()) : the_post(); ?>
+
+
+
+     <!-- <h1 class="entry-title"><?php the_title(); ?> "this is the title "</h1> -->
+      <?php #get_template_part('templates/entry-meta'); ?>
+
+       <div class="container homeCenter">
+
+            <div class="row">
+
+                 <ul class="sm-thumb-container">
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+                          <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                      <li class="sm-thumb-li"><img class ="thumb" src="/wp-content/themes/bensTheme/assets/images/thumbPH.png"></li>
+
+                </ul>
+
+
+                 <!-- main image -->
+
+            <div class="sm-main-image" style="border:solid;">
+
+                      <img class="mainImage" src ="/wp-content/themes/bensTheme/assets/images/thumbPH.png">
+            </div>
+
+           <!-- end main image -->
+                         </div>
+
+
+
+                            </div>
+
+                <!-- end desktop non bootstrap thumbnails -->
+
+
+
+
+                     </div>
+
+
+           </div>
+
+</div>
+           </div>
+ <footer>
+      <?php #wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+    </footer>
+<?php endwhile; ?>

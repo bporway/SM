aea3012367c90b41796b9a1840cbe91ee01700cf
Ben Porway <benporway@gmail.com>
2015-05-26 12:42:15 -0400

    junk commit

---
+++ wp-content/themes/bensTheme/page-news.php
@@ -0,0 +1,42 @@
+<div class="cushycms"><table border="0" cellpadding="10" cellspacing="0" width="800">
+
+	<tbody>
+
+		<tr valign="top">
+
+			<td width="330"><br>
+
+			<br>
+
+			<br>
+
+			<!-- ## IMAGE HERE ## -->
+
+			<div align="center"><img height="360" src="web/images/html/news_centerpiece.jpg" width="245"></div>
+
+			</td>
+
+			<td align="left" width="460"><img align="left" height="420" src="web/images/html/blank.gif" width="1"> <!-- ## CONTENT HERE ## -->
+
+			<p class="pageTitle">news</p>
+
+<div id="divRss"><ul class="feedEkList"><li><div class="itemTitle"><a href="http://www.stevemarselstudio.com/blog/chicks-with-guns/" target="_blank">Chicks with Guns – Seeing RED</a></div><div class="itemDate">2/9/2015</div></li><li><div class="itemTitle"><a href="http://www.stevemarselstudio.com/blog/don-zimmer-one-hell-life/" target="_blank">Don Zimmer: “One Hell of a Life”</a></div><div class="itemDate">11/20/2014</div></li><li><div class="itemTitle"><a href="http://www.stevemarselstudio.com/blog/salute-to-our-bravest/" target="_blank">Our Bravest: A Salute to Veterans</a></div><div class="itemDate">11/11/2014</div></li><li><div class="itemTitle"><a href="http://www.stevemarselstudio.com/blog/ice-holes-alex-sledding/" target="_blank">ICE HOLES – Alex Sledding</a></div><div class="itemDate">6/29/2014</div></li><li><div class="itemTitle"><a href="http://www.stevemarselstudio.com/blog/steve-marsel-cinemagraph-of-poles/" target="_blank">Steve Marsel~Cinemagraph of Poles</a></div><div class="itemDate">6/28/2014</div></li><li><div class="itemTitle"><a href="http://www.stevemarselstudio.com/blog/ice-holes-two-wise-men/" target="_blank">ICE HOLES – Two Wise Men?</a></div><div class="itemDate">6/28/2014</div></li><li><div class="itemTitle"><a href="http://www.stevemarselstudio.com/blog/steve-marsel-cinemagraph-lights/" target="_blank">Steve Marsel~Cinemagraph of Lights</a></div><div class="itemDate">6/28/2014</div></li><li><div class="itemTitle"><a href="http://www.stevemarselstudio.com/blog/ice-holes-kahtoola-to-the-rescue/" target="_blank">ICE HOLES – Kahtoola to the Rescue</a></div><div class="itemDate">6/26/2014</div></li></ul></div>
+			<!-- old news  -->
+			<script>
+			$('#divRss').FeedEk({
+FeedUrl: 'http://www.stevemarselstudio.com/blog/?feed=rss',
+DescCharacterLimit: 100,
+ShowDesc:false,
+MaxCount: 8,
+});
+			</script>
+			
+			</td>
+
+		</tr>
+
+	</tbody>
+
+</table>
+
+</div>

5a8d89915e3d839611103ab643bda4d8d458f647
Ben Porway <benporway@gmail.com>
2015-05-26 21:31:14 -0400

    making custom gallery page proper name

---
+++ wp-content/themes/bensTheme/single-image-sm-sm.php
@@ -0,0 +1,129 @@
+<?php while (have_posts()) : the_post(); ?>
+
+<?php
+
+$images = get_field('images');
+$count = 0 ;
+//echo count($images);
+
+?>
+
+     <!-- <h1 class="entry-title"><?php the_title(); ?> "this is the title "</h1> -->
+
+       <div class="container homeCenter">
+           <div class="clippingContainer">
+            <div class="innerContainer">
+                <div class="thumb_table">
+
+                <?php foreach($images as $image) : ?>
+
+                        <?php if ($count % 4 == 0 ) {
+                            echo "<div class=\"thumb_column\">";
+
+}
+
+                ?>
+
+
+                        <div class="thumb_row img-zoom"><img src="<?php echo $image['url'] ?>"></div>
+
+                <?php $count++; ?>
+                <?php if($count % 4 == 0 ) { echo "</div>";} ?>
+
+
+                <?php endforeach; ?>
+                        </div>
+
+
+
+</div>
+           </div>
+
+            </div>
+
+
+
+            <div class="sm-main-image" style="border:solid;">
+
+                      <img class="mainImage" src ="../wp-content/themes/bensTheme/thumbPH.png">
+            </div>
+
+           <!-- end main image -->
+                         </div>
+
+
+
+                            </div>
+
+                <!-- end desktop non bootstrap thumbnails -->
+
+
+
+
+                     </div>
+
+
+           </div>
+
+</div>
+           </div>
+ <footer>
+      <?php #wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+    </footer>
+<?php endwhile; ?>
+
+<script>
+
+$( document ).ready(function() {
+    //click for main image script
+
+$('.thumb_row').click(function(){
+    var $clickedImage =$(this).children('img').attr("src");
+   // alert($clickedImage);
+    $('.mainImage').attr("src", $clickedImage);
+});
+    //scrolling script on mouse
+var $div = $('.innerContainer');
+var $ul = $('.thumb_table');
+//alert($ul.width());
+// width of div
+var width = $div.width();
+
+// width of ul - width of div
+var ulWidth = $ul.width() - width;
+
+$div.on('mouseenter', function(e) {
+        // get left offset of div on page
+        var divLeft = $div.offset().left;
+
+
+        $(window).on('mousemove', function(e) {
+            var left = e.pageX - divLeft;
+            // get percent of width the mouse position is at
+            var percent = left / width;
+           // alert($ul.css('margin-left'));
+            // set margin-left on ul to achieve a 'scroll' effect
+
+            $ul.css('margin-left', -(percent * ulWidth));
+
+
+
+        });
+    }).on('mouseleave', function() {
+        // remove mousemove event
+        $(window).off('mousemove');
+    });
+
+
+    //zoom script
+
+
+    $('.img-zoom').hover(function() {
+        $(this).addClass('transition');
+
+    }, function() {
+        $(this).removeClass('transition');
+    });
+
+ });
+</script>

0ea652692ef33250fa737b2dbe1f282a86067b16
Bj <benporway@gmail.com>
2015-06-14 03:39:12 -0400

    contact page

---
+++ wp-content/themes/bensTheme/page-contact.php
@@ -0,0 +1,10 @@
+<?php while (have_posts()) : the_post(); ?>
+
+<div class = "homeCenter">
+
+</div>
+
+ <!-- footer -->
+    <div align="center" class="sm-footer"><div style="text-align: center;"><img border="0" height="27" src="http://stevemarselstudio.com/web/images/html/footer_phone.gif" width="119"><a href="#" onmouseout="swapImgRestore();" onmouseover="swapImage('footerEmail','','web/images/html/footer_email_rollover.gif',1);"><img border="0" height="27" id="footerEmail" name="footerEmail" src="http://stevemarselstudio.com/web/images/html/footer_email.gif" width="188"></a></div>
+</div>
+<?php endwhile; ?>

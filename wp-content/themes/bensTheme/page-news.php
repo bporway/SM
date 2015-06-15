06c8c18b163a6051b9bf060d1aa1806376d9160b
Bj <benporway@gmail.com>
2015-06-15 03:46:40 -0400

    news page

--- wp-content/themes/bensTheme/page-news.php
+++ wp-content/themes/bensTheme/page-news.php
@@ -38,5 +38,7 @@ $newsImage = get_field('newsimage');
 
 
      <!-- <h1 class="entry-title"><?php the_title(); ?> "this is the title "</h1> -->
-
+ <!-- footer -->
+    <div align="center" class="sm-footer"><div style="text-align: center;"><img border="0" height="27" src="http://stevemarselstudio.com/web/images/html/footer_phone.gif" width="119"><a href="#" onmouseout="swapImgRestore();" onmouseover="swapImage('footerEmail','','web/images/html/footer_email_rollover.gif',1);"><img border="0" height="27" id="footerEmail" name="footerEmail" src="http://stevemarselstudio.com/web/images/html/footer_email.gif" width="188"></a></div>
+</div>
 <?php endwhile; ?>
